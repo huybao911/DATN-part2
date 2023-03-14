8ac278c42dc7eadab5ca44c28c813d24ead2f7a7
huybao911 <huybaongotran6@gmail.com>
2023-03-15 02:44:09 +0700

    15/3

--- frontend/src/pages/admin/Users.tsx
+++ frontend/src/pages/admin/Users.tsx
@@ -2,14 +2,14 @@ import * as React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { useDispatch, useSelector } from "react-redux";
 
-import { getUsers } from "redux/actions/admin";
+import { getUsers, deleteUser } from "redux/actions/admin";
 import { RootState } from "redux/reducers";
 import { IAdmin } from "redux/types/admin";
 import { ISManager } from "redux/types/sManager";
 import { IManager } from "redux/types/Manager";
 import { IUser } from "redux/types/user";
-import { TableContainer } from "@material-ui/core";
-
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { Link } from 'react-router-dom';
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: "60%",
@@ -17,8 +17,8 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function refreshPage(){ 
-  window.location.reload(); 
+function refreshPage() {
+  window.location.reload();
 }
 
 const Users: React.FC = (): JSX.Element => {
@@ -87,11 +87,180 @@ const Users: React.FC = (): JSX.Element => {
     //       <button style={{fontSize:"20px", backgroundColor:"#000", color:"#fff",border:"10px solid black"}}>Thêm tòa nhà</button>
     //     </Link>
     //   </div>
-      
+
     // </div>
 
     <TableContainer>
+      {/* Table user */}
+      <Typography variant="h6" fontWeight="bold" sx={{ textAlign: "center", padding: "10px", marginTop: "10px" }} >User</Typography>
+      <Table component={Paper}>
+        <TableHead>
+          <TableRow >
+            <TableCell align="right">Tên User</TableCell>
+            <TableCell align="right">Email</TableCell>
+            <TableCell align="right">Role</TableCell>
+            <TableCell align="right">Department</TableCell>
+            <TableCell align="right" />
+          </TableRow>
+        </TableHead>
 
+        <TableBody>
+          {users.map((user: any) =>
+
+            <TableRow key={user.username}>
+              <TableCell align="right">
+                {user.username}
+              </TableCell>
+
+              <TableCell align="right">
+                {user.email}
+              </TableCell>
+
+              <TableCell align="right">
+                {user.role.keyRole}
+              </TableCell>
+
+              <TableCell align="right">
+                {user.department}
+              </TableCell >
+
+              <TableCell align="center">
+                {
+                  <Button
+                    type='button'
+                    variant='contained'
+                    color='secondary'
+                    size='small'
+                    onClick={(e) => dispatch(deleteUser(user._id))}
+                  >
+                    Xóa
+                  </Button>
+                }
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+
+      </Table>
+
+      {/* Table super manager */}
+      <Typography variant="h6" fontWeight="bold" sx={{ textAlign: "center", padding: "10px", marginTop: "10px" }} >Super Manager</Typography>
+      <Table component={Paper}>
+        <TableHead>
+          <TableRow >
+            <TableCell align="right">Tên User</TableCell>
+            <TableCell align="right">Email</TableCell>
+            <TableCell align="right">Role</TableCell>
+            <TableCell align="right">Department</TableCell>
+            <TableCell align="right" />
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          {SManagers.map((user: any) =>
+
+            <TableRow key={user.username}>
+              <TableCell align="right">
+                {user.username}
+              </TableCell>
+
+              <TableCell align="right">
+                {user.email}
+              </TableCell>
+
+              <TableCell align="right">
+                {user.role.keyRole}
+              </TableCell>
+
+              <TableCell align="right">
+                {user.department}
+              </TableCell >
+
+              <TableCell align="center">
+                {
+                  <Button
+                    type='button'
+                    variant='contained'
+                    color='secondary'
+                    size='small'
+                    onClick={(e) => dispatch(deleteUser(user._id))}
+                  >
+                    Xóa
+                  </Button>
+                }
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+
+      </Table>
+
+      {/* Table manager */}
+      <Typography variant="h6" fontWeight="bold" sx={{ textAlign: "center", padding: "10px", marginTop: "10px" }} >Manager</Typography>
+      <Table component={Paper}>
+        <TableHead>
+          <TableRow >
+            <TableCell align="right">Tên User</TableCell>
+            <TableCell align="right">Email</TableCell>
+            <TableCell align="right">Role</TableCell>
+            <TableCell align="right">Department</TableCell>
+            <TableCell align="right" />
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          {Managers.map((user: any) =>
+
+            <TableRow key={user.username}>
+              <TableCell align="right">
+                {user.username}
+              </TableCell>
+
+              <TableCell align="right">
+                {user.email}
+              </TableCell>
+
+              <TableCell align="right">
+                {user.role.keyRole}
+              </TableCell>
+
+              <TableCell align="right">
+                {user.department}
+              </TableCell >
+
+              <TableCell align="center">
+                {
+                  <Button
+                    type='button'
+                    variant='contained'
+                    color='secondary'
+                    size='small'
+                    onClick={(e) => dispatch(deleteUser(user._id))}
+                  >
+                    Xóa
+                  </Button>
+                }
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+
+      </Table>
+
+
+      {/* two button Create and Add */}
+      <div style={{ textAlign: "center", marginTop: "30px" }}>
+        <Link to="registerAdmin">
+          <button style={{ fontSize: "20px", backgroundColor: "#000", color: "#fff", border: "10px solid black" }}>TẠO TÀI KHOẢN</button>
+        </Link>
+      </div>
+
+      {/* button add */}
+      <div style={{ textAlign: "center", marginTop: "20px" }}>
+        <Link to="adddepartment">
+          <button style={{ fontSize: "20px", backgroundColor: "#000", color: "#fff", border: "10px solid black" }}>Thêm tòa nhà</button>
+        </Link>
+      </div>
     </TableContainer>
   );
 };
