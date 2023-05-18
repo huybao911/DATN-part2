3a1ac0041496e297a5164a94df4a6e2a337d42d1
huybao911 <huybaongotran6@gmail.com>
2023-05-18 23:27:15 +0700

    update 18/5/2023

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 mongoose.set('strictQuery', true);
+
+dotenv.config({ path: "./.env" });
 
 module.exports = async (server) => {
   try {
